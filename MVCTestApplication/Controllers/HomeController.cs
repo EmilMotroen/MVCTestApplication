a521e777a0e3e93cc0e5fb2f61e829316c2b2d72
Emil Motrøen <e.motroen@gmail.com>
2024-03-18 20:04:10 +0100

    Add project files.

---
+++ MVCTestApplication/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCTestApplication.Models;
+using System.Diagnostics;
+
+namespace MVCTestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HomeModel model = new HomeModel();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult IncrementCount()
+        {
+            model.IncrementCount();
+
+            return RedirectToAction("Counter");
+        }
+
+        public ActionResult Counter()
+        {
+            return View(model);
+        }
+    }
+}
---
+++ MVCTestApplication/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTestApplication.Models
+{
+    public class HomeModel
+    {
+        public int CurrentCount { get; set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
